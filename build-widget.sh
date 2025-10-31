a5a373e24a5678a7a4c212a2ae3b6b368e0db413
vovkvant <vovkvant@gmail.com>
2025-10-31 18:46:09 +0100

    Fix build script

--- build-widget.sh
+++ build-widget.sh
@@ -6,7 +6,7 @@ GASTBOT_WIDGET_FOLDER="/home/gastbot/back/gastbot-widget-api"
 git pull
 export MAVEN_OPTS="--add-opens=java.base/java.lang=ALL-UNNAMED"
 mvn -DskipTests=true clean install
-cp gastbot-widget/target/gastbot-widget-api-1.0-SNAPSHOT.jar $GASTBOT_WIDGET_FOLDER
+cp target/gastbot-widget-api-1.0-SNAPSHOT.jar $GASTBOT_WIDGET_FOLDER
 
 cp devops/start.sh $GASTBOT_WIDGET_FOLDER
 cp devops/stop.sh $GASTBOT_WIDGET_FOLDER
